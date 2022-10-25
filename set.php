a671ee4b4bd35489ad7e514817bc045f417838c2
PrathameshBhagat <90595097+PrathameshBhagat@users.noreply.github.com>
2022-10-25 23:13:47 +0530

    Create set.php

---
+++ set.php
@@ -0,0 +1,10 @@
+<?php
+//this saves the code and sets it in a file
+$_POST = json_decode(file_get_contents('php://input'), true);
+$script=$_POST["script"];
+$uname=$_POST["username"];
+$file = fopen('./text/'.$uname.".txt", "w") or die("Unable to open file!");
+fwrite($file, $script);
+fclose($file);
+echo "baapaaay(saved)script:".$_POST["script"].",uname:".$_POST["username"];
+?>

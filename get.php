ceff74779c276677c47578ada69e59d26c283a54
PrathameshBhagat <90595097+PrathameshBhagat@users.noreply.github.com>
2022-10-28 14:47:45 +0530

    Update get.php

--- get.php
+++ get.php
@@ -5,14 +5,15 @@ $uname=$_GET["username"];
 $lang=$_GET["language"];
 //READ FILE
 $myfile = fopen('./text/'.$uname.".txt", "r") or die("Unable to open file!");
-echo "<h1>";
 //HTTP REQUEST TO GET BYTECODE
 $url = "https://api.jdoodle.com/v1/execute";
 $curl = curl_init($url);
 curl_setopt($curl, CURLOPT_URL, $url);
 curl_setopt($curl, CURLOPT_POST, true);
 curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-$scr=fread($filer,filesize("a.txt"));
+$scr=fread($myfile,filesize('./text/'.$uname.".txt"));
+$scr=str_replace("\n","\\n", $scr);
+$scr=str_replace("\"","\\\"", $scr);
 $headers = array(
    "Content-Type: application/json",
 );
@@ -21,7 +22,7 @@ $data = '{"clientId": "bad4b3695b0c1e45eeb5ade8be594c4d","clientSecret":"354b658
 $data.=$lang;
 //SET language
 $data.='","script":"';
-$data.=fread($myfile,filesize('./text/'.$uname.".txt"));//set script
+$data.=$scr;//set script
 $data.='","versionIndex":"0"}';
 curl_setopt($curl, CURLOPT_POSTFIELDS, $data);
 //for debug only!
@@ -29,11 +30,12 @@ curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, false);
 curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
 $resp = curl_exec($curl);
 curl_close($curl);
-var_dump($resp);
-
+$r=json_decode($resp);
+// OUTPUT
+if (isset($r->output))  echo $r->output;
+if (isset($r->error))   echo $r->error;
+//var_dump();
 //HTTP REQUEST END
-
-
 
 fclose($filer);
 ?>

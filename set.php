dd6bc4b3e482de814d8934eb6ec3aa94803fb188
PrathameshBhagat <90595097+PrathameshBhagat@users.noreply.github.com>
2022-10-28 14:49:16 +0530

    Update set.php

--- set.php
+++ set.php
@@ -1,9 +1,14 @@
 <?php
-$_POST = json_decode(file_get_contents('php://input'), true);
+/*$_POST = json_decode(file_get_contents('php://input'), true);
 $script=$_POST["script"];
-$uname=$_POST["username"];
+$uname=$_POST["username"];*/
+$uname=$_GET["username"];
+$p=file_get_contents('php://input');
 $file = fopen('./text/'.$uname.".txt", "w") or die("Unable to open file!");
-fwrite($file, $script);
+//fwrite($file, $script);
+fwrite($file, $p);
 fclose($file);
-echo "baapaaay(saved)script:".$_POST["script"].",uname:".$_POST["username"];
+//echo "baapaaay(saved)script:".$_POST["script"].",uname:".$_POST["username"];
+
+echo "baapaaay(saved)script:".$p.",uname:".$uname;
 ?>

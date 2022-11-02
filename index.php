b94a2534f66305a94ad5ee1b9b20a873bf05c2e3
PrathameshBhagat <90595097+PrathameshBhagat@users.noreply.github.com>
2022-11-02 19:29:39 +0530

    Update index.php

--- index.php
+++ index.php
@@ -2,19 +2,37 @@
   <head>    
     <title>Document</title>
     <link rel="stylesheet" href="style.css" />
+    <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3506596207312408"
+     crossorigin="anonymous"></script>
   </head>
-  <body><div id="N1" align="left" ></div><div id="N2"  ></div>
+  <body><div id="N1" align="left" ></div><div id="N2" style="left:10px" ></div><script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3506596207312408"
+     crossorigin="anonymous"></script>
     <form>
-        <h3 class="question">Write a program in hindi we will get it converted into java/python/c++/js/php code<br>
+        <h3 class="question">Write a program <--in hindi we will get it converted into java/python/c++/js/php code--></--in><br>
             <INPUT TYPE="TEXT" name="uname" placeholder="prathamesh " >&nbsp
-            <INPUT TYPE="TEXT" name="lang" placeholder="java">
+            <select name="lang"><option value="java">JAVA</option><option value="python">PYTHON</option><option value="php">PHP</option><option value="javascript">JAVASCRIPT</option><option value="c">C</option></select>
              <input type="button" value="SAVE" onclick="save()">
             <input type="button" value="RUN" onclick="run()">
         </h3>
     <div class="editor-container" id="mycode">
-         <div type=text id="editor" name="pl">public class Main {public static void main(String [] a){System.out.println("hello world");}
-}</div><h3></h3>
-        <div id="view"  style="height:100%" class=" ace_editor ace-cobalt ace_dark"></div>
+         <div type=text id="editor" name="pl"><!--सार्वजनिक कक्षा  Main {सार्वजनिक स्थिर रिक्त जरूरी (डोर  [] a){    अगर (44>3 )System.out.println("hell ld");}}}-->/*THIS IS A CUSTOM CODE EDITOR FOR A NEW PROGRAMMING LANGUAGE 
+IT HAS COMPLETELY DIFFERENT SYNTAXING AND EFFICIENT COMPILATION TIME.
+THE DECODER ,COMPILER ,AND VIRTUAL MACINE IS READY.
+JUST WAITING FOR DOCUMENTATION TO COMPLETE AFTER WHICH WE WOULD LAUNCH IT 
+IT WOULD HAVE A CODE EDITOR SPECIFIC TO THE LANGUAGE.
+THE LANGUAGE WOULD HAVE PROPER DOCUMENTATION WITH DETAILED YOUTUBE VIDEOS TO TEACH THE LANGUAGE.
+WE ALSO HAVE PLANNED TO CREATE A VISUAL AND ANDROID STUIDO PLUGIN TO SUPPORT THE LANGUAGE. 
+PLEASE WAIT FOR SOME TIME UNTIL THE DOCUMENTATION IS COMPLETE 
+AS OF NOW IT JUST RUNS PROGRAMS OF LANGUAGES LIKE JAVA PHP C JS ETC
+Please apporove it as soon as possible so that the original site data,code other files can be added */
+public class Main{
+         public static void main (String []args){
+         System.out.println("This site can have ads form online placement courses,B.E.,Btech and other colleges,companies seeking low cost programmers ,other Youtube channels,etc ");
+         System.out.println("Please apporove it as soon as possible so that the original site sample code,data and other  files can be added");
+}
+}
+         </div><h3></h3>
+        <div id="view"  style="height:100%;font:150% monospace;color:#ff0" class=" ace_editor ace-cobalt ace_dark"></div>
     </div>
         </form>
      <script src="https://cdnjs.cloudflare.com/ajax/libs/ace/1.12.3/ace.js" integrity="sha512-yARx+3W/tyZPXyRfZ4DLRdj0rXF2yjH2D6bKpPslrl1c62Q6ZC808L++ft9jkzIN9vmLtQCFsYNrzoOE/Im2Dg==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>   <script src="main.js"></script>

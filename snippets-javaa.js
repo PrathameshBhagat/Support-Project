62eaa3e9f12409d5db05a4454f3f689fe1bcc529
PrathameshBhagat <90595097+PrathameshBhagat@users.noreply.github.com>
2022-11-16 23:23:08 +0530

    Create snippets-javaa.js

---
+++ snippets-javaa.js
@@ -0,0 +1,12 @@
+ace.define("ace/snippets/javaa",["require","exports","module"], function(require, exports, module) {
+  "use strict";
+
+exports.snippetText = "snippet cs1\n\
+\tcase\n\
+\t\twhen ${1:when} then ${2:then}\n\
+\t\tdefault ${3:इंट}\n\
+\tend\n\
+snippet nak\n\tअंक\n\snippet is\n\tइंस\n\
+";
+ exports.scope = "javaa";
+});

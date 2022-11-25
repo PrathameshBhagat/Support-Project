49a9738f7a221f1421bf5cc940f3a93fea005c59
PrathameshBhagat <90595097+PrathameshBhagat@users.noreply.github.com>
2022-11-25 13:14:33 +0530

    Update decode.php

--- decode.php
+++ decode.php
@@ -6,69 +6,17 @@ $l=$_GET["lang"];
     global $s;$s= str_replace($h,$e,$s); 
 }
 if($l=="java"){
-    //OOP common keywords 
-r("कक्षा","class");r("समूह","class");       
-r("सार्वजनीक","public");r("सार्वजनिक","public");
-r("प्रिंट","print");
-//pure java keywords
-r("स्थिर","static");       
-r("रिक्त","void");
-r("जरूरी","main");  
-r("तंत्र","System");       
-r("बाहर","out");
-r("आम","default");
-r("अंतिम","final");
-///////////////////////r("","println");
-
-
-// Data Types d for double is at last  
-r("अंक","int");
-r("बाइट","byte");
-r("छोटा","short");
-r("लंबा","long");
-r("फ्लोट","float");
-r("बड़ा","double");
-r("वर्ण","char");
-r("डोर","String");
-r("बूलियन","bollean");
-// Data Types 
-
-/*
-r("","");
-r("","");*/
-//Control Statments
-//r("","");
-r("अगर","if");       
-r("या","else");
-r("बदलो","switch");r("बदले","switch");       
-r("यदि","case");
-r("केलिए","for");     
-r("जब-तक","while");r("जबतक","while");r("जब","while");
-r("करो","do");
-r("जारी","continue");
-r("भंग","break");
-r("आम","default");
-r("","");
-
-//single words and booleans
-r("ल","l");
-r("ब","d");
-r("फ","f");
+  
 r("सच","true");
 r("","");
 r("",""); 
 
 }  /**/
 if($l=="php"){
-r("कक्षा","class");r("समूह","class");       
-r("सार्वजनीक","public");r("सार्वजनिक","public");
 
 //r("","");
 r("अगर","if");       
-r("वरना","else");
-r("बदलो","switch");r("बदल","switch");       
-r("हैतो","case");r("हैतो","case");
-r("केलिए","for");r("के लिए","for");     
+r("वरना","else");      
 r("जबतक ","while");r("जबतक ","while");
 r("करो","do"); 
     

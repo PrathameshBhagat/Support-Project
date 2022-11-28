4614ee69e346f00eae0a280ec61d5aac1ad1a15a
PrathameshBhagat <90595097+PrathameshBhagat@users.noreply.github.com>
2022-11-29 02:27:27 +0530

    Add files via upload

---
+++ Hindi4all.java
@@ -0,0 +1,74 @@
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.Scanner;
+import javax.net.ssl.HttpsURLConnection;
+import java.nio.charset.StandardCharsets; 
+import java.io.*;
+public class Hindi4all  implements Runnable{
+	private static int done=0;
+	private String cname="";
+	private String read(String name){
+		try {
+		FileReader fr = new FileReader(name);
+		int r = 0;String read="";
+		while ((r = fr.read()) != -1) {read+=(char) r;}
+		return read;
+	    }
+	    catch(Exception e) {System.out.println("Exception Occurred:");e.printStackTrace();} 
+	    return ""; 
+	}
+	private void writeIt(String name ,String data){
+		if(data!=""&&data!=null&&name!=""&&name!=null) try { 
+      	FileOutputStream fos = new FileOutputStream(name,false);//true to append
+      	byte[] b =data.getBytes(); //converts string into bytes  
+      	fos.write(b); //writes bytes into file  
+      	fos.close(); //close the file  
+    	}
+    	catch(Exception e) {System.out.println("Exception Occurred:");e.printStackTrace();}  
+		else {System.out.println("File name "+name+"or Contents"+data+" not found (or is empty/null)");}
+	}
+	private  String process(String data ){
+		try{
+			URL url = new URL("https://crptrest.000webhostapp.com/decode.php?lang=java");
+			HttpsURLConnection http = (HttpsURLConnection)url.openConnection();
+			http.setRequestMethod("POST");
+			http.setDoOutput(true);
+			http.setRequestProperty("Content-Type", "text/plain");
+			byte[] out = data.getBytes(StandardCharsets.UTF_8);
+			OutputStream stream = http.getOutputStream();
+			stream.write(out);
+			 InputStreamReader isr=new InputStreamReader(http.getInputStream());
+			BufferedReader br = new BufferedReader(isr);
+			StringBuffer sb =new StringBuffer();
+			String input;			
+	   		while ((input = br.readLine()) != null){
+	      	sb.append(input+"\n");
+	   		}
+	   		br.close();
+			http.disconnect();return sb.toString();
+		} catch (MalformedURLException e) {
+         System.out.println("Internet is not connected");
+      } catch (IOException e) {
+         System.out.println("Internet is not connected");
+      }catch(Exception e) {System.out.println("Exception Occurred:");e.printStackTrace();}  
+		return null;
+	}
+	@Override
+	public void run() {writeIt(cname,process(read(cname)));this.done++;}
+	Hindi4all(int hjghjhj){}
+	public Hindi4all(String ... a){
+	    for (int i=0;i<a.length;i++ )
+	    {	//writeIt(a[i],process(read(a[i])));
+	    	Hindi4all h=new Hindi4all(0);
+	    	h.cname=a[i];
+	    	Thread t= new Thread(h);
+		t.start();
+    	}
+    	String jkbk="";
+    	//While loop to stop main from returning before other threads get completed 
+    	while(true){jkbk=String.valueOf(this.done)+a.length;if(this.done>=a.length)break;else {}}
+	    	//System.out.println("\tthis.done"+this.done+"\ta.length"+a.length);
+	}
+}
